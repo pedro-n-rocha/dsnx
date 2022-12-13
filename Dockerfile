b22d651bc520a26e16dd8896efb75edbe941c50a
Pedro Rocha <procha@epsilon.pt>
2022-12-13 10:47:13 +0000

    cleanups

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,3 @@ ADD snx /usr/sbin
 ADD snxrc.tmpl /root/snxrc.tmpl
 ADD root.db /etc/snx/
 ADD USER.db /etc/snx/
-
-ADD init.sh /init.sh
-
-
-CMD ["/bin/sh", "-c", "envsubst < /root/snxrc.tmpl >/root/.snxrc && /init.sh"]
--- init.sh
+++
@@ -1,5 +0,0 @@
-echo $PASSWORD | snx
-iptables -t nat -A POSTROUTING -o tunsnx -j MASQUERADE
-iptables -A FORWARD -i tunsnx -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o tunsnx -j ACCEPT
-tail -f /dev/null
